6786c992286aaba5377ba55eb80c887c273f3d1b
Michael Stenta <mike@mstenta.net>
2020-01-03 15:21:38 -0500

    Add more Feeds tests for unusual values.

--- tests/src/Unit/Feeds/Target/FractionTargetTest.php
+++ tests/src/Unit/Feeds/Target/FractionTargetTest.php
@@ -33,10 +33,48 @@ class FractionTargetTest extends FieldTargetTestBase {
     ];
     $target = new FractionTarget($configuration, 'fraction', []);
     $method = $this->getProtectedClosure($target, 'prepareValue');
-    $values = ['value' => '1/10'];
+
+    // Test a basic fraction: 1/2.
+    $values = ['value' => '1/2'];
     $method(0, $values);
     $this->assertSame($values['numerator'], '1');
-    $this->assertSame($values['denominator'], '10');
+    $this->assertSame($values['denominator'], '2');
+
+    // Test that a zero numerator results in an empty fraction.
+    $values = ['value' => '0/1'];
+    $method(0, $values);
+    $this->assertSame($values['numerator'], '');
+    $this->assertSame($values['denominator'], '');
+
+    // Test that a zero denominator results in an empty fraction.
+    $values = ['value' => '1/0'];
+    $method(0, $values);
+    $this->assertSame($values['numerator'], '');
+    $this->assertSame($values['denominator'], '');
+
+    // Test that a negative numerator works.
+    $values = ['value' => '-1/2'];
+    $method(0, $values);
+    $this->assertSame($values['numerator'], '-1');
+    $this->assertSame($values['denominator'], '2');
+
+    // Test that a negative denominator results in an empty fraction.
+    $values = ['value' => '1/-2'];
+    $method(0, $values);
+    $this->assertSame($values['numerator'], '');
+    $this->assertSame($values['denominator'], '');
+
+    // Test that a decimal value results in an empty fraction.
+    $values = ['value' => '1.0'];
+    $method(0, $values);
+    $this->assertSame($values['numerator'], '');
+    $this->assertSame($values['denominator'], '');
+
+    // Test that a non-numeric value results in an empty fraction.
+    $values = ['value' => 'te/st'];
+    $method(0, $values);
+    $this->assertSame($values['numerator'], '');
+    $this->assertSame($values['denominator'], '');
   }
 
   /**
@@ -51,10 +89,36 @@ class FractionTargetTest extends FieldTargetTestBase {
     ];
     $target = new FractionTarget($configuration, 'fraction', []);
     $method = $this->getProtectedClosure($target, 'prepareValue');
-    $values = ['value' => 0.55];
+
+    // Test a basic decimal.
+    $values = ['value' => 0.5];
     $method(0, $values);
-    $this->assertSame($values['numerator'], '55');
-    $this->assertSame($values['denominator'], '100');
+    $this->assertSame($values['numerator'], '5');
+    $this->assertSame($values['denominator'], '10');
+
+    // Test a negative decimal.
+    $values = ['value' => -1];
+    $method(0, $values);
+    $this->assertSame($values['numerator'], '-1');
+    $this->assertSame($values['denominator'], '1');
+
+    // Test that a zero value results in 0/1.
+    $values = ['value' => 0];
+    $method(0, $values);
+    $this->assertSame($values['numerator'], '0');
+    $this->assertSame($values['denominator'], '1');
+
+    // Test that an empty value results in an empty fraction.
+    $values = ['value' => ''];
+    $method(0, $values);
+    $this->assertSame($values['numerator'], '');
+    $this->assertSame($values['denominator'], '');
+
+    // Test that a string value results in an empty fraction.
+    $values = ['value' => 'test'];
+    $method(0, $values);
+    $this->assertSame($values['numerator'], '');
+    $this->assertSame($values['denominator'], '');
   }
 
   /**
