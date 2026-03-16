4bbb89daff5c45ed36fc1d0c357fa525ecd2e1d6
Michael Stenta <mike@mstenta.net>
2026-03-16 17:34:55 -0400

    Issue #3579598: Deprecate Drupal 7 fraction migration field plugin

--- src/Plugin/migrate/field/d7/FractionField.php
+++ src/Plugin/migrate/field/d7/FractionField.php
@@ -7,6 +7,10 @@ use Drupal\migrate_drupal\Plugin\migrate\field\FieldPluginBase;
 /**
  * Migrate from d7 fraction structure.
  *
+ * @deprecated in fraction:3.1.0 and is removed from fraction:4.0.0.
+ *   Drupal 7 is no longer supported.
+ * @see https://www.drupal.org/node/3579599
+ *
  * @MigrateField(
  *   id = "fraction",
  *   core = {7},
